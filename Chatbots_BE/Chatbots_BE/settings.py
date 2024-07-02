f104a0cc484974e8a6f3d96285860c34a6562933
Divy Patel <125264776+Divy0409@users.noreply.github.com>
2024-07-02 02:15:49 -0400

    minor bug fix

--- Chatbots_BE/Chatbots_BE/settings.py
+++ Chatbots_BE/Chatbots_BE/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ChatBots_FE','build','static'),
+]
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,8 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
